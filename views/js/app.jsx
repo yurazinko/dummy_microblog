e8c7e6f19695edc137bc3d73ec05b5681448c09f
Yurii Zinko <yurii.zinko@vertalab.com>
2018-08-17 12:25:42 +0300

    Add logged in and home component

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -1,5 +1,45 @@
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-8 col-xs-offset-2 jumbotron text-center">
+          <h1>Dummy Microblog</h1>
+          <p>Sign in to get access </p>
+          <a onClick={this.authenticate} className="btn btn-primary btn-lg btn-login btn-block">Sign In</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+class LoggedIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="col-lg-12">
+          <br />
+          <span className="pull-right"><a onClick={this.logout}>Log out</a></span>
+          <h2>Dummy microblog</h2>
+          <p>Lets write something</p>
+          <div className="row">
+            {this.state.jokes.map(function(joke, i){
+              return (<Joke key={i} joke={joke} />);
+            })}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
-  
   render() {
     if (this.loggedIn) {
       return (<LoggedIn />);
